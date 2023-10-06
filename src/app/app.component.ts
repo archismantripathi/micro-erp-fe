d434540e305a5a86f40411c30f1444bf96243231
Archisman Tripathi <archismantripathi@gmail.com>
2023-10-06 10:29:57 +0530

    add input element

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss','./shared/stylesheets/elements.scss']
 })
 export class AppComponent {
   title = 'micro-erp';
